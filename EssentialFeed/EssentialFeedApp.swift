ee70f62b53c9ded994bd85d01f6bcf81a247d140
Balasnest <balasnest@gmail.com>
2024-05-04 12:59:10 +0530

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by BM on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
