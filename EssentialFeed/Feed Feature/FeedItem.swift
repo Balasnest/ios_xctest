ad5e3d7fda5cc02cd8bee38f261bf583673821cc
Balasnest <balasnest@gmail.com>
2024-05-04 15:20:53 +0530

    Create Feed feature, map its file

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by BM on 04/05/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let uid: UUID
+    let description: String?
+}
---
+++ EssentialFeed/Feed Feature/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by BM on 04/05/24.
+//
+
+import Foundation
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
